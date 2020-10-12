efdbec39e349f34bafaca9ed0ac306a550f77237
Alex <32b3@protonmail.com>
2020-10-12 12:39:05 +0200

    service interface defined

---
+++ product/service/service.go
@@ -0,0 +1,17 @@
+package service
+
+import (
+	"github.com/alkmc/restClean/product/entity"
+
+	"github.com/google/uuid"
+)
+
+//Service is responsible for interaction with Repository interface
+type Service interface {
+	Validate(p *entity.Product) error
+	Create(p *entity.Product) (*entity.Product, error)
+	FindByID(id uuid.UUID) (*entity.Product, error)
+	FindAll() ([]entity.Product, error)
+	Update(p *entity.Product) error
+	Delete(id uuid.UUID) error
+}
